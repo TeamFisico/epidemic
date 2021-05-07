c018e70ec91b8360d291b519755c5c96d18c24d6
Niccolo Zanotti <niccolo.zanotti@icloud.com>
2021-05-07 23:30:14 +0200

    Clang format and CMake implementation-->build with make clangformat to format the code

--- apps/main_rks.cpp
+++ apps/main_rks.cpp
@@ -6,74 +6,87 @@
 #include <iomanip>
 #include <iostream>
 
-
 /////////  ROOT  ///////
-#include "TF1.h"
 #include "TApplication.h"
-#include "TGraph.h"
 #include "TCanvas.h"
+#include "TF1.h"
+#include "TGraph.h"
 #include "TMultiGraph.h"
 
-
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     using namespace seir;
 
-    //default simulation parameters
-    int pop {0};
-    int sim_time {0};
-    double refresh {0.0}; //time step
-    double param1 {0.0};  //beta
-    double param2 {0.0}; //alpha
-    double param3 {0.0}; //gamma
-    //initialState
-    State start {(double)pop,0.0,1.0,0.0};
+    // default simulation parameters
+    int pop{0};
+    int sim_time{0};
+    double refresh{0.0}; // time step
+    double param1{0.0};  // beta
+    double param2{0.0};  // alpha
+    double param3{0.0};  // gamma
+    // initialState
+    State start{(double)pop, 0.0, 1.0, 0.0};
     ode conditions;
 
-    //input quest
+    // input quest
     std::string answer;
-    std::cout << "Do you want to set the simulation parameters yourself?" <<std::endl;
+    std::cout << "Do you want to set the simulation parameters yourself?" << std::endl;
     std::cout << "Type y or n." << std::endl;
     //////////FARE ERROR CHECKING (COME CHIAMARE ISVALID)
-    while(std::cin >> answer){
-      if (answer == "y" || answer == "yes"){
-        std::cout << std::setw(15) << "Population : "; std::cin >> pop;
-        std::cout << std::setw(15)  << "Simulation time(days) : "; std::cin>> sim_time;
-        std::cout << std::setw(15)  << "Time step(days) : "; std::cin>> refresh;
-        std::cout << std::setw(15) << "Parameter beta : "; std::cin >> param1;
-        std::cout << std::setw(15) << "Parameter alpha : "; std::cin >> param2;
-        std::cout << std::setw(15) << "Parameter gamma : "; std::cin>> param3;
-        std::cout << std::setw(15) << "Susceptible individuals : "; std::cin>> start.S;
-        std::cout << std::setw(15) << "Latent individuals : "; std::cin>> start.E;
-        std::cout << std::setw(15) << "Infected individuals : "; std::cin>> start.I;
-        std::cout << std::setw(15) << "Removed individuals : "; std::cin>> start.R;
-        conditions = {pop,sim_time,refresh,start,param1,param2,param3};
-        break;
-      } else if(answer == "n" || answer == "no"){
-        break;
-      }
-
+    while (std::cin >> answer)
+    {
+        if (answer == "y" || answer == "yes")
+        {
+            std::cout << std::setw(15) << "Population : ";
+            std::cin >> pop;
+            std::cout << std::setw(15) << "Simulation time(days) : ";
+            std::cin >> sim_time;
+            std::cout << std::setw(15) << "Time step(days) : ";
+            std::cin >> refresh;
+            std::cout << std::setw(15) << "Parameter beta : ";
+            std::cin >> param1;
+            std::cout << std::setw(15) << "Parameter alpha : ";
+            std::cin >> param2;
+            std::cout << std::setw(15) << "Parameter gamma : ";
+            std::cin >> param3;
+            std::cout << std::setw(15) << "Susceptible individuals : ";
+            std::cin >> start.S;
+            std::cout << std::setw(15) << "Latent individuals : ";
+            std::cin >> start.E;
+            std::cout << std::setw(15) << "Infected individuals : ";
+            std::cin >> start.I;
+            std::cout << std::setw(15) << "Removed individuals : ";
+            std::cin >> start.R;
+            conditions = {pop, sim_time, refresh, start, param1, param2, param3};
+            break;
+        }
+        else if (answer == "n" || answer == "no")
+        {
+            break;
+        }
     }
     Simulation mysimulation;
 
-    simulation(conditions,mysimulation);
-     std::ofstream out{"output.txt"};
+    simulation(conditions, mysimulation);
+    std::ofstream out{"output.txt"};
 
-  std::cout << "┌─────┬───────────────┬───────────────┬───────────────┬───────────────┐" << std::endl;
-  std::cout << "│ Day │       S       │       E       │       I       │       R       │" << std::endl;
-  std::cout << "├─────├───────────────├───────────────├───────────────├───────────────├" << std::endl;
-  int t1 = 0;
-  for (auto& a: mysimulation) {
-    out << "S = " << a.S << " E = " << a.E << " I = " << a.I << " R = " << a.R << std::endl;
+    std::cout << "┌─────┬───────────────┬───────────────┬───────────────┬───────────────┐" << std::endl;
+    std::cout << "│ Day │       S       │       E       │       I       │       R       │" << std::endl;
+    std::cout << "├─────├───────────────├───────────────├───────────────├───────────────├" << std::endl;
+    int t1 = 0;
+    for (auto &a : mysimulation)
+    {
+        out << "S = " << a.S << " E = " << a.E << " I = " << a.I << " R = " << a.R << std::endl;
 
-    std::cout << std::setprecision(9) << std::left << "│" << std::setw(5) << t1 << "│" << std::setw(15) << a.S << "│" << std::setw(15) << a.E << "│" << std::setw(15) << a.I << "│" << std::setw(15) << a.R << "│" << std::endl;
-    t1++;
-  }
-  std::cout << "└─────┴───────────────┴───────────────┴───────────────┴───────────────┘" << std::endl;
+        std::cout << std::setprecision(9) << std::left << "│" << std::setw(5) << t1 << "│" << std::setw(15) << a.S
+                  << "│" << std::setw(15) << a.E << "│" << std::setw(15) << a.I << "│" << std::setw(15) << a.R << "│"
+                  << std::endl;
+        t1++;
+    }
+    std::cout << "└─────┴───────────────┴───────────────┴───────────────┴───────────────┘" << std::endl;
 
-    //ROOT CODE
+    // ROOT CODE
     TApplication app("app", &argc, argv);
-
 
     auto c0 = new TCanvas("c0", "Epidemic evolution");
     auto mg = new TMultiGraph();
@@ -88,18 +101,23 @@ int main (int argc, char* argv[])
     mg->SetTitle("Evolution; time (days); number of people");
 
     int t = 0;
-    for(auto a: mysimulation){
-        gS->SetPoint(t,t, a.S);
-        gE->SetPoint(t,t, a.E);
-        gI->SetPoint(t,t, a.I);
-        gR->SetPoint(t,t, a.R);
+    for (auto a : mysimulation)
+    {
+        gS->SetPoint(t, t, a.S);
+        gE->SetPoint(t, t, a.E);
+        gI->SetPoint(t, t, a.I);
+        gR->SetPoint(t, t, a.R);
         t++;
     }
 
-    mg->Add(gS); gS->SetTitle("S");
-    mg->Add(gE); gE->SetTitle("E");
-    mg->Add(gI); gI->SetTitle("I");
-    mg->Add(gR); gR->SetTitle("R");
+    mg->Add(gS);
+    gS->SetTitle("S");
+    mg->Add(gE);
+    gE->SetTitle("E");
+    mg->Add(gI);
+    gI->SetTitle("I");
+    mg->Add(gR);
+    gR->SetTitle("R");
 
     mg->Draw("AL");
     c0->BuildLegend();
@@ -107,11 +125,9 @@ int main (int argc, char* argv[])
     c0->Modified();
     c0->Update();
 
-//    TRootCanvas *rc = (TRootCanvas *)c0->GetCanvasImp();
-//    rc->Connect("CloseWindow()", "TApplication", gApplication, "Terminate()");
+    //    TRootCanvas *rc = (TRootCanvas *)c0->GetCanvasImp();
+    //    rc->Connect("CloseWindow()", "TApplication", gApplication, "Terminate()");
     app.Run();
 
     return 0;
-
-
-    }
+}
--- apps/main_std1.cpp
+++ apps/main_std1.cpp
@@ -1,90 +1,100 @@
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <stdexcept>
 #include "epidemic1/SEIR.hpp"
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <stdexcept>
 
 #include <TApplication.h>
+#include <TCanvas.h>
 #include <TGraph.h>
 #include <TMultiGraph.h>
-#include <TCanvas.h>
 #include <TRootCanvas.h>
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
-    int pop {0};
-    int sim_time {100};
-    double param1 {0.0};  //beta
-    double param2 {0.0}; //alpha
-    double param3 {0.0}; //gamma
+    int pop{0};
+    int sim_time{100};
+    double param1{0.0}; // beta
+    double param2{0.0}; // alpha
+    double param3{0.0}; // gamma
     State state0{};
     sim ode;
 
     std::string answer;
-    std::cout << "Do you want to set the simulation parameters yourself?" <<std::endl;
+    std::cout << "Do you want to set the simulation parameters yourself?" << std::endl;
     std::cout << "Type y or n." << std::endl;
 
-    try {
-      while (std::cin >> answer) {
-        if (answer == "y" || answer == "yes") {
-          std::cout << std::setw(15) << "Population : ";
-          std::cin >> pop;
-          std::cout << std::setw(15) << "Simulation time(days) : ";
-          std::cin >> sim_time;
-          std::cout << std::setw(15) << "Parameter beta : ";
-          std::cin >> param1;
-          std::cout << std::setw(15) << "Parameter alpha : ";
-          std::cin >> param2;
-          std::cout << std::setw(15) << "Parameter gamma : ";
-          std::cin >> param3;
-          std::cout << std::setw(15) << "Susceptible individuals : ";
-          std::cin >> state0.S;
-          std::cout << std::setw(15) << "Latent individuals : ";
-          std::cin >> state0.E;
-          std::cout << std::setw(15) << "Infected individuals : ";
-          std::cin >> state0.I;
-          std::cout << std::setw(15) << "Removed individuals : ";
-          std::cin >> state0.R;
-          ode = {state0, param2, param1, param3};
-          if (pop !=
-              static_cast<int>(state0.S + state0.E + state0.I + state0.R)) {
-            throw std::runtime_error{
-                "ERROR: The sum of individuals is different from the popolation"};
-          } else {
-            break;
-          }
-        } else if (answer == "n" || answer == "no") {
-          break;
+    try
+    {
+        while (std::cin >> answer)
+        {
+            if (answer == "y" || answer == "yes")
+            {
+                std::cout << std::setw(15) << "Population : ";
+                std::cin >> pop;
+                std::cout << std::setw(15) << "Simulation time(days) : ";
+                std::cin >> sim_time;
+                std::cout << std::setw(15) << "Parameter beta : ";
+                std::cin >> param1;
+                std::cout << std::setw(15) << "Parameter alpha : ";
+                std::cin >> param2;
+                std::cout << std::setw(15) << "Parameter gamma : ";
+                std::cin >> param3;
+                std::cout << std::setw(15) << "Susceptible individuals : ";
+                std::cin >> state0.S;
+                std::cout << std::setw(15) << "Latent individuals : ";
+                std::cin >> state0.E;
+                std::cout << std::setw(15) << "Infected individuals : ";
+                std::cin >> state0.I;
+                std::cout << std::setw(15) << "Removed individuals : ";
+                std::cin >> state0.R;
+                ode = {state0, param2, param1, param3};
+                if (pop != static_cast<int>(state0.S + state0.E + state0.I + state0.R))
+                {
+                    throw std::runtime_error{"ERROR: The sum of individuals is different from the popolation"};
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else if (answer == "n" || answer == "no")
+            {
+                break;
+            }
+            else
+            {
+                throw std::runtime_error{"ERROR: input not supported"};
+            }
         }
-        else{
-          throw std::runtime_error{"ERROR: input not supported"};
-        }
-      }
-    } catch (std::runtime_error const& e){
-      std::cerr << e.what() << std::endl;
-      return 1;
+    }
+    catch (std::runtime_error const &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
     std::vector<State> result = ode.generate_all_points(sim_time);
 
-
-    std::ofstream out {"output.txt"};
+    std::ofstream out{"output.txt"};
     std::cout << "┌─────┬───────────────┬───────────────┬───────────────┬───────────────┐" << std::endl;
     std::cout << "│ Day │       S       │       E       │       I       │       R       │" << std::endl;
     std::cout << "├─────├───────────────├───────────────├───────────────├───────────────├" << std::endl;
     int t1 = 0;
-    for (auto& a: result) {
-      out << "S = " << a.S << " E = " << a.E << " I = " << a.I << " R = " << a.R << std::endl;
+    for (auto &a : result)
+    {
+        out << "S = " << a.S << " E = " << a.E << " I = " << a.I << " R = " << a.R << std::endl;
 
-      std::cout << std::setprecision(9) << std::left << "│" << std::setw(5) << t1 << "│" << std::setw(15) << a.S << "│" << std::setw(15) << a.E << "│" << std::setw(15) << a.I << "│" << std::setw(15) << a.R << "│" << std::endl;
-      t1++;
+        std::cout << std::setprecision(9) << std::left << "│" << std::setw(5) << t1 << "│" << std::setw(15) << a.S
+                  << "│" << std::setw(15) << a.E << "│" << std::setw(15) << a.I << "│" << std::setw(15) << a.R << "│"
+                  << std::endl;
+        t1++;
     }
     std::cout << "└─────┴───────────────┴───────────────┴───────────────┴───────────────┘" << std::endl;
 
-    //ROOT CODE
+    // ROOT CODE
 
     TApplication app("app", &argc, argv);
-
 
     auto c0 = new TCanvas("c0", "Evoluzione");
     auto mg = new TMultiGraph();
@@ -99,26 +109,31 @@ int main (int argc, char* argv[])
     mg->SetTitle("Evolution; time (days); number of people");
 
     int t2 = 0;
-    for(auto& a: result){
-        gS->SetPoint(t2,t2, a.S);
-        gE->SetPoint(t2,t2, a.E);
-        gI->SetPoint(t2,t2, a.I);
-        gR->SetPoint(t2,t2, a.R);
+    for (auto &a : result)
+    {
+        gS->SetPoint(t2, t2, a.S);
+        gE->SetPoint(t2, t2, a.E);
+        gI->SetPoint(t2, t2, a.I);
+        gR->SetPoint(t2, t2, a.R);
         t2++;
     }
 
-    mg->Add(gS); gS->SetTitle("S");
-    mg->Add(gE); gE->SetTitle("E");
-    mg->Add(gI); gI->SetTitle("I");
-    mg->Add(gR); gR->SetTitle("R");
+    mg->Add(gS);
+    gS->SetTitle("S");
+    mg->Add(gE);
+    gE->SetTitle("E");
+    mg->Add(gI);
+    gI->SetTitle("I");
+    mg->Add(gR);
+    gR->SetTitle("R");
 
     mg->Draw("AL");
     c0->BuildLegend();
 
     c0->Modified();
     c0->Update();
-//    TRootCanvas *rc = (TRootCanvas *)c0->GetCanvasImp();
-//    rc->Connect("CloseWindow()", "TApplication", gApplication, "Terminate()");
+    //    TRootCanvas *rc = (TRootCanvas *)c0->GetCanvasImp();
+    //    rc->Connect("CloseWindow()", "TApplication", gApplication, "Terminate()");
     app.Run();
 
     return 0;
