18657a316de3022f42d9c6213912c5ad6dc34878
Filippo Pretolani <filippopretolani88@gmail.com>
2021-06-21 21:14:12 +0200

    Added random class

---
+++ src/random.cpp
@@ -0,0 +1,38 @@
+#include "../include/random.hpp"
+#include <cassert>
+
+namespace smooth_simulation
+{
+/////////////////////////////////////////////////////
+////////     RANDOM DEFAULT CONSTRUCTOR       ///////
+/////////////////////////////////////////////////////
+Random::Random()
+{
+    mt19937_rng default_engine; // default seed engine with 256 bits of entropy (See
+                                // https://www.pcg-random.org/posts/simple-portable-cpp-seed-entropy.html)
+    eng = default_engine;
+}
+
+double Random::uniform(double lower, double upper)
+{
+    return eng.uniform(lower, upper);
+}
+int Random::int_uniform(int lower, int upper)
+{
+    return eng.variate<int, std::uniform_int_distribution>(lower, upper);
+}
+double Random::gauss(double mean, double stddev)
+{
+    return eng.variate<double, std::normal_distribution>(mean, stddev);
+}
+int Random::rounded_gauss(double mean, double stddev)
+{
+    return round(eng.variate<double, std::normal_distribution>(mean, stddev));
+}
+int Random::piecewise(std::vector<int> nums, std::vector<double> weights)
+{
+    return eng.variate<int, std::piecewise_constant_distribution>(std::begin(nums), std::end(nums),
+                                                                  std::begin(weights));
+}
+
+} // namespace smooth_simulation
