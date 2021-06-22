fe475886bed0fc96c7c06178440d96689666b428
Filippo Pretolani <filippopretolani88@gmail.com>
2021-06-22 22:16:55 +0200

    Added ifdef guard to parameters.hpp

--- include/epidemic2/parameters.hpp
+++ include/epidemic2/parameters.hpp
@@ -1,3 +1,6 @@
+#ifndef PARAMETERS_HPP
+#define PARAMETERS_HPP
+
 double constexpr TRANSMISSION_RANGE = 0.5;
 double constexpr HOME_RADIUS = 0.5;
 double constexpr HOME_PROBABILITY = 0.5;
@@ -5,3 +8,5 @@ double constexpr PI = 3.14159265358979323846;
 double constexpr PAUSE_EXPONENT = 0.5;
 double constexpr MAX_PAUSE = 60.0;
 double constexpr MIN_PAUSE = 5.0;
+
+#endif
