884cc8a81670cb2a594db5d2676622b70e95d27b
Filippo Pretolani <filippopretolani88@gmail.com>
2021-07-04 09:51:57 +0200

    Removed the slashes in cmake -S ../ because it was not working

--- scripts/def-sim.sh
+++ scripts/def-sim.sh
@@ -1,5 +1,5 @@
 mkdir ../sim_build
-cmake -S ../ -B ../sim_build
+cmake -S .. -B ../sim_build
 cd ../sim_build || exit
 make sim-graphics
 echo "Target successfully built"
--- scripts/testing.sh
+++ scripts/testing.sh
@@ -1,5 +1,5 @@
 mkdir ../test_build
-cmake -S ../ -B ../test_build 
+cmake -S .. -B ../test_build
 cd ../test_build
 make seir.t
 echo "target successfully built"
