68d8f016444798ca3dda1f0b83f5e43b48e97930
Filippo Pretolani <filippopretolani88@gmail.com>
2021-07-06 18:37:37 +0200

    Rearranged linkin order in CMakeLists.txt; added a test in test2.cpp, still not complete

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,17 +1,51 @@
-#include "world.hpp"
+#include "simulation.hpp"
 #include <iostream>
 #include "doctest.h"
 
 using namespace smooth_sim;
 
-TEST_CASE("Path generation test")
+TEST_CASE("Path cleaning test")
 {
-    World prova{1000,5,1000,10000,323,433,542};
-    mobility_model& per = prova.Clusters()[0].population()[0];
-    std::vector<double> weights(5,1);
-    Random rng;
-    prova.generate_path(5,weights,per.path(),rng);
-    for(auto& a: per.path()){
-        std::cout << "X = " << a->pos().get_x() << " Y = " << a->pos().get_x() << std::endl;
+    Simulation Test(10000, 0, 0, 0,10,1000,1000,0.1,0.1,0.1,1,100);
+    for(unsigned int i = 0; i < 10; ++i) //move ten times
+    {
+        Test.move();
     }
+    //Change the color of 5 clusters to Yellow
+    for(unsigned int i = 0; i < 10; i += 2){
+        Test.world().clusters()[i].set_zone(Zone::Yellow);
+    }
+    //Clean the path based on cluster color
+    for(auto& cl: Test.world().clusters()){
+        for(auto& person: cl.people()){
+            if(Test.world().clusters()[person.cluster_index()].get_zone() == Zone::Green){
+                Test.clean_path(person); //To make public for testing
+            }
+            else{
+                Test.clean_cluster_path(person); //TODO add this function from the other branch
+            }
+        }
+    }
+
+    int counter = 0;
+    for(auto& cl: Test.world().clusters()){
+        for(auto& person: cl.people()){
+            if(Test.world().clusters()[person.cluster_index()].get_zone() == Zone::Green){ //if the person is in a green cluster
+                for(auto& loc: person.path()){
+                    if(Test.world().clusters()[loc->get_cluster_index()].get_zone() != Zone::Green){ //check if all location are valid
+                        ++counter;
+                    }
+                }
+            }
+            else{ //if the person is not in a green cluster
+                for(auto& loc: person.path()){
+                    if(loc->get_cluster_index() != person.cluster_index()){ //check if all location are valid
+                        ++counter;
+                    }
+                }
+            }
+        }
+    }
+
+    CHECK(counter == 0);
 }
